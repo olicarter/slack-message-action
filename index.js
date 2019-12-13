ff69fc874f305ec39c253d00b5497c2d1f032624
Oli Carter <oli@seenit.io>
2019-12-13 16:02:51 +0000

    attempt random GIFs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fetch = require('node-fetch');
 
 const {
   env: {
+    GIPHY_API_KEY,
     GITHUB_ACTOR,
     GITHUB_REF,
     GITHUB_REPOSITORY,
@@ -16,36 +17,63 @@ const {
 const branchName = GITHUB_REF.replace(/refs\/heads\//, '');
 
 // Get blocks input JSON string and replace vars
-const blocks = core
+let blocks = core
   .getInput('blocks')
   .replace(/{{\s*BRANCH\s*}}/g, branchName)
   .replace(/{{\s*ACTOR\s*}}/g, GITHUB_ACTOR)
   .replace(/{{\s*REPOSITORY\s*}}/g, GITHUB_REPOSITORY)
   .replace(/{{\s*SHA\s*}}/g, GITHUB_SHA);
 
-// Create Slack message object
-const body = {
-  channel: SLACK_CHANNEL,
-  attachments: [{ blocks: JSON.parse(blocks) }],
-};
+const GIFKeywordRegex = /{{\s*GIF\|(\w+)\s*}}/g;
+const blocksContainsGIF = GIFKeywordRegex.test(blocks);
 
-// Send Slack message
-(async () => {
-  console.log('Sending message');
-  try {
-    await fetch('https://slack.com/api/chat.postMessage', {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers: {
-        Authorization: `Bearer ${SLACK_BOT_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-    });
-    console.log('Message sent');
-    process.exit(0);
-  } catch (err) {
-    console.error(err.message);
-    console.error(err.response.data);
-    process.exit(1);
+// Replace GIF variables with random GIF URLs
+if (blocksContainsGIF) {
+  let GIFKeywords = GIFKeywordRegex.exec(blocks);
+  var promises = [];
+  while (GIFKeyword !== null) {
+    const GIFKeyword = GIFKeywords[0];
+    promises.push(
+      new Promise(async function(resolve) {
+        const res = await fetch(
+          `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${GIFKeyword}&limit=1`,
+          { method: 'GET' },
+        );
+        const { data } = await res.json();
+        const GIFURL = data[0].url;
+        blocks.replace(GIFKeywordRegex, GIFURL);
+        GIFKeywords = GIFKeywordRegex.exec(blocks);
+        resolve();
+      }),
+    );
   }
-})();
+}
+
+Promise.all(promises).then(function() {
+  // Create Slack message object
+  const body = {
+    channel: SLACK_CHANNEL,
+    attachments: [{ blocks: JSON.parse(blocks) }],
+  };
+
+  // Send Slack message
+  (async () => {
+    console.log('Sending message');
+    try {
+      await fetch('https://slack.com/api/chat.postMessage', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+          Authorization: `Bearer ${SLACK_BOT_TOKEN}`,
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log('Message sent');
+      process.exit(0);
+    } catch (err) {
+      console.error(err.message);
+      console.error(err.response.data);
+      process.exit(1);
+    }
+  })();
+});
