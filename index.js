7a53d65df00c5ba8e02f04e0de5b5b93f765b8d4
Oliver Carter <olicartr@gmail.com>
2019-12-17 10:37:50 +0000

    remove gif logic

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fetch = require('node-fetch');
 
 const {
   env: {
-    GIPHY_API_KEY,
     GITHUB_ACTOR,
     GITHUB_REF,
     GITHUB_REPOSITORY,
@@ -24,54 +23,29 @@ let blocks = core
   .replace(/{{\s*REPOSITORY\s*}}/g, GITHUB_REPOSITORY)
   .replace(/{{\s*SHA\s*}}/g, GITHUB_SHA);
 
-const GIFKeywordRegex = /{{\s*GIF\|(\w+)\s*}}/g;
+// Create Slack message object
+const body = {
+  channel: SLACK_CHANNEL,
+  attachments: [{ blocks: JSON.parse(blocks) }],
+};
 
-// Replace GIF variables with random GIF URLs
-let GIFKeywords = GIFKeywordRegex.exec(blocks);
-var promises = [];
-while (GIFKeywords !== null) {
-  const GIFKeyword = GIFKeywords[1];
-  promises.push(
-    new Promise(async function(resolve) {
-      const res = await fetch(
-        `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${GIFKeyword}&limit=1`,
-        { method: 'GET' },
-      );
-      const { data } = await res.json();
-      const GIFURL = data[0].url;
-      blocks.replace(GIFKeywordRegex, GIFURL);
-      resolve();
-    }),
-  );
-  GIFKeywords = GIFKeywordRegex.exec(blocks);
-}
-
-Promise.all(promises).then(function() {
-  // Create Slack message object
-  const body = {
-    channel: SLACK_CHANNEL,
-    attachments: [{ blocks: JSON.parse(blocks) }],
-  };
-
-  // Send Slack message
-  (async () => {
-    console.log('Sending message');
-    console.log('body', JSON.stringify(body));
-    try {
-      await fetch('https://slack.com/api/chat.postMessage', {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-          Authorization: `Bearer ${SLACK_BOT_TOKEN}`,
-          'Content-Type': 'application/json',
-        },
-      });
-      console.log('Message sent');
-      process.exit(0);
-    } catch (err) {
-      console.error(err.message);
-      console.error(err.response.data);
-      process.exit(1);
-    }
-  })();
-});
+// Send Slack message
+(async () => {
+  console.log('Sending message');
+  try {
+    await fetch('https://slack.com/api/chat.postMessage', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        Authorization: `Bearer ${SLACK_BOT_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+    });
+    console.log('Message sent');
+    process.exit(0);
+  } catch (err) {
+    console.error(err.message);
+    console.error(err.response.data);
+    process.exit(1);
+  }
+})();
